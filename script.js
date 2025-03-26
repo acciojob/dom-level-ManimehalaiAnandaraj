75c78c1ddc888df1d5f65a07fa76914071bd0704
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-26 11:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-function domlevelfind() {
+function domlevelfind(level) {
 	const element=document.getElementById("level");
 	if(!element)
 	{
-		return "Element not found"
+		alert( "Element not found" + level +"not found");
 	}
 	let count=1;
 	let current=element;
@@ -11,20 +11,6 @@ function domlevelfind() {
 		count++;
 		current += parrentNode;
 	}
-	return count;
+	alert("The level of the element is"+count);
 }
-
-const elemid="count";
-const domlevel=domlevelfind(elemid);
-if(domlevel !== "Element not found")
-{
-	alert("The level of the element is: " +domlevel);
-}
-
-else
-{
-	alert(domlevel)
-}
-
-
 
